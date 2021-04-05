8684738fd38c0454de0335012f8d04fb17f650cb
Karan Singh Singare <karansingare@gmail.com>
2021-04-05 16:14:44 +0530

    integrated the login/authentication

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import ContactForm, LoginForm, RegisterForm
 
@@ -39,8 +40,17 @@ def login_page(request):
     print(request.user.is_authenticated)
     if form.is_valid():
         print(form.cleaned_data)
-        # resetting the form with empty context
-        data['form'] = LoginForm()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            user = authenticate(request, username=username, password=password)
+            # data['form'] = LoginForm()
+            return redirect('/login')
+        else:
+            # Return an 'invalid login' error message.
+            print("Error")
     return render(request, 'auth/login.html', context=data)
 
 def register_page(request):
